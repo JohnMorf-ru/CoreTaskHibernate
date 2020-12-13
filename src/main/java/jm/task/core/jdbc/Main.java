f78959ef116c2553eb74862d4cc7ad24818687ca
JohnMorf <johnmorf1507@gmail.com>
2020-12-13 23:10:51 +0300

    Complete 1.1.3 JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.dropUsersTable();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+
+        List<User> list = Arrays.asList(new User("Jessy", "Pinkman", (byte) 29)
+                , new User("Walter", "White", (byte) 53)
+                , new User("Soul", "Goodman", (byte) 49)
+                , new User("Gustavo", "Frink", (byte) 50));
+        list.forEach((x) -> userService.saveUser(x.getName(), x.getLastName(), x.getAge()));
+
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,89 @@
 package jm.task.core.jdbc.dao;
 
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = Util.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(createTableSQL);
+        } catch (MySQLSyntaxErrorException ex) {
+            System.out.println("users уже создана");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = Util.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.execute(dropTableSQL);
+        } catch (MySQLSyntaxErrorException ex) {
+            System.out.println("Такая база уже не существует");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Connection connection = Util.getConnection()) {
+            PreparedStatement pstatement = connection.prepareStatement(insertUserSQL);
+            pstatement.setString(1, name);
+            pstatement.setString(2, lastName);
+            pstatement.setInt(3, age);
+            pstatement.execute();
+            System.out.printf("User с именем – %s добавлен в базу данных\n", name);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection connection = Util.getConnection()) {
+            PreparedStatement prstatement = connection.prepareStatement(removeByIdSQL);
+            prstatement.setString(1, String.valueOf(id));
+            prstatement.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try(Connection connection = Util.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getAllUsersSQL);
+            while (resultSet.next()){
+                long id = resultSet.getInt("user_id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                User user = new User(name, lastName, age);
+                user.setId(id);
+                users.add(user);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try (Connection connection = Util.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(cleanTableSQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
